90140f99e8db4592c50823f5b645c9a56415bda9
Luandos <luandosssantos9@gmail.com>
2026-09-18 11:21:23 -0300

    Estrutura do monitoramento incompleto

--- pensamento_computacional/Monitoramento_Temperaturas/main.c
+++ pensamento_computacional/Monitoramento_Temperaturas/main.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main()
+{
+    
+   float limite,temp,soma,media,maior=0,menor=0;    //Declaracao de var
+   int contli,contador,consecutiva;
+   printf("Iniciando...");                          //Começo de interação
+   printf("Digite o limite de temperatura");
+   scanf("%f".&limite);
+   do{                                              //Estrutura Principal de analise de temperatura
+       printf("Monioramento..");
+       printf("Digite a temperatura da maquina em ºC: ");
+       scanf("%f". &temp);
+       soma=soma+temp;
+       if (temp >= limite){
+        contli++;
+       }
+       if (temp > maior);{
+        maior=temp;
+       }
+       if (temp < menor){
+           menor=temp;
+       }
+       
+       contador=contador++;
+   } while (contli<3);
+   media=soma/contador;
+   printf("Resultados");
+   printf("menor temperatura: %fC".menor);
+   printf("Maior temperatura: %fC".maior);
+   printf("Media: %f", media);
+   printf("Quantidade de limites atigidos: %f",contli);
+    return 0;
+}
