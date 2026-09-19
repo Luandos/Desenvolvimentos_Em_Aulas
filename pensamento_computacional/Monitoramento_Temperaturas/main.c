418b622ddfb841215eabcffa3b3c255374c1476f
Luandos <luandosssantos9@gmail.com>
2026-09-19 15:18:55 -0300

    Refactor temperature monitoring logic and output

--- pensamento_computacional/Monitoramento_Temperaturas/main.c
+++ pensamento_computacional/Monitoramento_Temperaturas/main.c
@@ -3,33 +3,50 @@
 int main()
 {
     
-   float limite,temp,soma,media,maior=0,menor=0;    //Declaracao de var
-   int contli,contador,consecutiva;
-   printf("Iniciando...");                          //Começo de interação
-   printf("Digite o limite de temperatura");
-   scanf("%f".&limite);
-   do{                                              //Estrutura Principal de analise de temperatura
-       printf("Monioramento..");
-       printf("Digite a temperatura da maquina em ºC: ");
-       scanf("%f". &temp);
+   float limite,temp,soma=0,media,maior=0,menor=0;    //Declaracao de var
+   int contli=0,contador=0;
+   
+    printf("Iniciando...\n");                          //Começo de interação
+    printf("Digite o limite de temperatura: ");
+    while (scanf("%f", &limite) != 1) {
+        printf("Valor invalido! Digite um numero: ");
+    while (getchar() != '\n');
+}
+   
+   while (contli != 3){
+      printf("Digite a temperatura atual: ");
+      while (scanf("%f", &temp) != 1){
+          printf("Valor invalido, tente novamente: ");
+      while (getchar() != '\n');
+   }
        soma=soma+temp;
-       if (temp >= limite){
-        contli++;
+       if (contador == 0){
+           menor = temp;
        }
-       if (temp > maior);{
-        maior=temp;
+       if (temp > maior){
+           maior = temp;
        }
        if (temp < menor){
-           menor=temp;
+           menor = temp;
        }
-       
-       contador=contador++;
-   } while (contli<3);
+       if (temp > limite){
+           contli++;
+           if (contli == 3){
+               printf("Limite exedido, programa irá se encerrar ...\n");
+           }
+       }
+       else{
+           contli=0;
+       }
+       contador++;
+   }
+
    media=soma/contador;
-   printf("Resultados");
-   printf("menor temperatura: %fC".menor);
-   printf("Maior temperatura: %fC".maior);
-   printf("Media: %f", media);
-   printf("Quantidade de limites atigidos: %f",contli);
+  
+   printf("Resultados\n"); //Saidas 
+   printf("Menor temperatura: %.2f°C\n",menor);
+   printf("Maior temperatura: %.2f°C\n",maior);
+   printf("Media: %.2f\n", media);
+   printf("Quantidade de vezes monitorado: %d\n",contador);
     return 0;
 }
